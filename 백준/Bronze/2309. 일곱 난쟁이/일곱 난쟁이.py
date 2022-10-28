c6adb6e782113c71fc38855a79b922bb16b60e59
Bosun-Kwak <62944371+Bosun-Kwak@users.noreply.github.com>
2022-10-28 15:26:11 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,27 +1,18 @@
-import sys
-input = sys.stdin.readline
+from itertools import combinations
 
-num_list =[]
-for i in range(9):
-  num_list.append(int(input()))
+lst = []
+for _ in range(9):
+    lst.append(int(input()))
+lst.sort()
+res = combinations(lst,7)
+r_lst = []
+for r in res:
+    sum = 0
+    for i in range(7):
+        sum += r[i]
+    if sum == 100:
+        r_lst = r
+        break
 
-# num_list = [20,7,23,19,10,15,25,8,13]
-num_list.sort()
-sum = sum(num_list[:])
-# print(sum)
-
-
-for i in range(8):
-  for j in range(i+1,9):
-    sum2 = sum
-    sum2 = sum2 - num_list[i]-num_list[j]
-    if sum2 == 100:
-      # num_list.remove(num_list[i])
-      # num_list.remove(num_list[j-1])
-      no_1 = i
-      no_2 = j
-      break
-
-for i in range(9):
-  if i != no_1 and i != no_2:
-    print(num_list[i])
+for i in range(7):
+    print(r[i])
